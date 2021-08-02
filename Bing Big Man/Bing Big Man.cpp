33db3d23717308da3cb62fb78dc37428483b77a0
Ricky Chuang <songsongyyohya@gmail.com>
2021-08-02 22:58:24 +0800

    Create Bing Big Man.cpp

---
+++ Bing Big Man/Bing Big Man.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std;
+
+int steps(int floor)
+{
+    if(floor==1) return 1;
+    if(floor==2) return 2;
+    return steps(floor-1)+steps(floor-2);
+}
+
+int main()
+{
+    int floor;
+    cin >> floor;
+    cout << steps(floor);
+  
+    return 0;
+}
