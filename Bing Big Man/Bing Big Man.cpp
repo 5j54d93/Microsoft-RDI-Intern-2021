e2a7785b7c4d7cc1c8e2547ec7b4a6f3f41480da
Ricky Chuang <songsongyyohya@gmail.com>
2021-08-02 23:21:28 +0800

    Update Bing Big Man.cpp

--- Bing Big Man/Bing Big Man.cpp
+++ Bing Big Man/Bing Big Man.cpp
@@ -5,12 +5,13 @@ int steps(int floor)
 {
     if(floor==1) return 1;
     if(floor==2) return 2;
-    return steps(floor-1)+steps(floor-2);
+    return steps(floor-1) + steps(floor-2);
 }
 
 int main()
 {
     int floor;
+    
     cin >> floor;
     cout << steps(floor);
   
